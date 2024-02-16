53be5763be92c81cf9da23630abefaa71aec7ba3
abdulrg01 <abdulrg01@gmail.com>
2024-02-16 21:17:35 +0100

    redux

--- client/redux/api/apiSlice.js
+++ client/redux/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:9000/api/v1/",
+    baseUrl: "https://eclass-erbc.onrender.com",
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
